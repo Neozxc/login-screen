3212f485288867be9f35f4c332a97e34c9056f0e
Nedas <neduska991@gmail.com>
2022-04-06 21:09:04 +0100

    .

--- src/components/Border/Border.js
+++ src/components/Border/Border.js
@@ -5,19 +5,19 @@ export const Border = () => {
   return (
     <div className='borderContainer'>
         {/* <div className="middleLine"></div> */}
-        <h1>Welcome Back!</h1>
+          <h1>Welcome Back!</h1>
 
 
-        <form action="submit">
-          <div className="usernameInput">
-            <input type="text" placeholder='Username' />
-          </div>
-          <div className="passwordInput">
-            <input type="text" placeholder='Password' />
-          </div>
-        </form>
+          <form action="submit">
+            <div className="usernameInput">
+              <input type="text" placeholder='Username' />
+            </div>
+            <div className="passwordInput">
+              <input type="text" placeholder='Password' />
+            </div>
+          </form>
 
-          <button>Login</button>
+            <button>Login</button>
     </div>
   )
 }
